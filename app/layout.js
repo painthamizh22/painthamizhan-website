51afef9a9a43bf76c333875b7bc0ed7d6902c3aa
Painthamizhan <painthamizhan22@gmail.com>
2026-09-05 09:15:38 +0530

    Add portfolio layout

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Painthamizhan S | Finance & Investment',
+  description: 'Portfolio of Painthamizhan S, B.Com Finance & Investment student at Christ University.'
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
